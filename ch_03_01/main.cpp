204a57405e1e9788b3e408aa2ec459e20105ff6b
= <seiya.kumada@gmail.com>
2019-10-31 12:16:49 +0000

    aeを実装中。

---
+++ ch_03_01/main.cpp
@@ -0,0 +1,38 @@
+#include <torch/torch.h>
+#include <string>
+#include <iostream>
+
+namespace
+{
+    const std::string DATA_DIR_PATH {"/home/ubuntu/data/mnist"};
+    constexpr int64_t TRAIN_BATCH_SIZE {64};
+    constexpr int64_t TEST_BATCH_SIZE {64};
+}
+
+int main(int argc, const char* argv[])
+{
+    auto train_dataset = torch::data::datasets::MNIST(
+        DATA_DIR_PATH,
+        torch::data::datasets::MNIST::Mode::kTrain)
+        .map(torch::data::transforms::Stack<>());
+
+    const size_t train_dataset_size = train_dataset.size().value();
+    std::cout << train_dataset_size << std::endl;
+
+    auto train_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
+        std::move(train_dataset), 
+        TRAIN_BATCH_SIZE);
+
+    auto test_dataset = torch::data::datasets::MNIST(
+        DATA_DIR_PATH, 
+        torch::data::datasets::MNIST::Mode::kTest)
+        .map(torch::data::transforms::Stack<>());
+
+    const size_t test_dataset_size = test_dataset.size().value();
+    std::cout << test_dataset_size << std::endl;
+    
+    auto test_loader = torch::data::make_data_loader(std::move(test_dataset), TEST_BATCH_SIZE);
+
+
+    return 0;
+}
