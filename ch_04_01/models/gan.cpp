8800e9850603f0677cb251b4b778790e8f54bf9f
= <seiya.kumada@gmail.com>
2020-02-20 14:07:29 +0000

    ch4_1を実装中。

--- ch_04_01/models/gan.cpp
+++ ch_04_01/models/gan.cpp
@@ -1,14 +1,14 @@
 #include "gan.h"
 
 GANImpl::Params::Params(
-    int                     start_channels,
-    const std::vector<int>& conv_filters,
-    const std::vector<int>& kernel_size,
-    const std::vector<int>& strides,
-    const double&           batch_norm_momentum,
-    const std::string&      activation,
-    const double&           dropout_rate,
-    const double&           learning_rate
+    int                             start_channels,
+    const std::vector<int>&         conv_filters,
+    const std::vector<int>&         kernel_size,
+    const std::vector<int>&         strides,
+    const boost::optional<double>&  batch_norm_momentum,
+    const std::string&              activation,
+    const boost::optional<double>&  dropout_rate,
+    const double&                   learning_rate
 )
     : start_channels_{start_channels}
     , conv_filters_{conv_filters}
@@ -26,7 +26,7 @@ GANImpl::Params::Params(
 GANImpl::GANImpl(
     const Params&           discriminator_params,
     const Params&           generator_params,
-    int                     generator_initial_dense_layer_size,
+    const std::vector<int>& generator_initial_dense_layer_size,
     const std::vector<int>& generator_upsample,
     const std::string&      optimizer,
     int                     z_dim,
@@ -50,13 +50,29 @@ void GANImpl::build()
     build_adversarial();
 }
 
-void GANImpl::build_discriminator()
+namespace
+{
+    inline torch::nn::Functional get_activation(const std::string& name)
+    {
+        if (name == "leaky_relu")
+        {
+            return torch::nn::Functional(torch::leaky_relu, 0.2);
+        }
+        else
+        {
+            return torch::nn::Functional(torch::relu);
+        }
+    }
+}
+
+torch::nn::Sequential GANImpl::build_discriminator()
 {
     torch::nn::Sequential discriminator {};
     auto in_channels = discriminator_params_.start_channels_;
+    auto out_channels = 0;
     for (auto i = 0; i < discriminator_params_.n_layers_; ++i)
     {
-        auto out_channels = discriminator_params_.conv_filters_[i];
+        out_channels = discriminator_params_.conv_filters_[i];
         auto c = torch::nn::Conv2d(
             torch::nn::Conv2dOptions(in_channels, out_channels, discriminator_params_.kernel_size_[i])
                 .stride(discriminator_params_.strides_[i])
@@ -64,7 +80,34 @@ void GANImpl::build_discriminator()
         );
         torch::nn::init::normal_(c->weight, 0, 0.2);
         torch::nn::init::zeros_(c->bias);
+        discriminator->push_back(std::move(c));
+        
+        if (discriminator_params_.batch_norm_momentum_ && i > 0)
+        {
+            auto b = torch::nn::BatchNorm(
+                torch::nn::BatchNormOptions(out_channels).momentum(discriminator_params_.batch_norm_momentum_.value())
+            );
+            discriminator->push_back(std::move(b));
+        }
+        discriminator->push_back(get_activation(discriminator_params_.activation_));
+
+        if (discriminator_params_.dropout_rate_)
+        {
+            discriminator->push_back(torch::nn::Dropout(discriminator_params_.dropout_rate_.value()));
+        }
+        in_channels = out_channels;
     }
+    
+    discriminator->push_back(torch::nn::Functional(torch::flatten, 1, -1));
+    
+    auto l = torch::nn::Linear(out_channels, 1);
+    torch::nn::init::normal_(l->weight, 0, 0.2);
+    torch::nn::init::zeros_(l->bias);
+    discriminator->push_back(std::move(l));
+    
+    discriminator->push_back(torch::nn::Functional(torch::sigmoid));
+
+    return discriminator;
 }
 
 void GANImpl::build_generator()
@@ -77,6 +120,44 @@ void GANImpl::build_adversarial()
 
 #if(UNIT_TEST_GAN)
 #include <boost/test/unit_test.hpp>
+
+namespace
+{
+    void test_0()
+    {
+        GANImpl::Params discriminator_params {
+            1, // start_channels
+            {64, 64, 128, 128}, // conv_filters
+            {5, 5, 5, 5}, // kernel_size
+            {2, 2, 2, 1}, // strides             
+            boost::none, // batch_norm_momentum
+            "relu", // activation          
+            0.4, // dropout_rate       
+            0.0008 // learning_rate  
+        };
+
+        GANImpl::Params generator_params {
+            1, // start_channels
+            {128, 64, 64, 1}, // conv_filters
+            {5, 5, 5, 5}, // kernel_size
+            {1, 1, 1, 1}, // strides             
+            0.9, // batch_norm_momentum
+            "relu", // activation          
+            boost::none, // dropout_rate       
+            0.0004 // learning_rate  
+        };
+
+        GAN gan {
+            discriminator_params,
+            generator_params,
+            std::vector<int>{7, 7, 64}, // generator_initial_dense_layer_size
+            std::vector<int>{2, 2, 1, 1}, // generator_upsample,
+            "rmsprop", // optimizer,
+            100, // z_dim,
+            torch::kCPU
+        };
+    }
+}
 
 BOOST_AUTO_TEST_CASE(TEST_GAN)
 {
--- ch_04_01/models/gan.h
+++ ch_04_01/models/gan.h
@@ -1,37 +1,39 @@
 #ifndef GAN_INCLUDE 
 #define GAN_INCLUDE
 #include <torch/torch.h>
+#include <boost/optional.hpp>
+#include <initializer_list>
 
 class GANImpl : public torch::nn::Module
 {
 public:
     struct Params
     {
-        int                 start_channels_;
-        std::vector<int>    conv_filters_;
-        std::vector<int>    kernel_size_;
-        std::vector<int>    strides_;
-        double              batch_norm_momentum_;
-        std::string         activation_;
-        double              dropout_rate_;
-        double              learning_rate_;
-        std::size_t         n_layers_;
+        int                     start_channels_;
+        std::vector<int>        conv_filters_;
+        std::vector<int>        kernel_size_;
+        std::vector<int>        strides_;
+        boost::optional<double> batch_norm_momentum_;
+        std::string             activation_;
+        boost::optional<double> dropout_rate_;
+        double                  learning_rate_;
+        std::size_t             n_layers_;
 
         Params(
-            int                     start_channels,
-            const std::vector<int>& conv_filters,
-            const std::vector<int>& kernel_size,
-            const std::vector<int>& strides,
-            const double&           batch_norm_momentum,
-            const std::string&      activation,
-            const double&           dropout_rate,
-            const double&           learning_rate);
+            int                             start_channels,
+            const std::vector<int>&         conv_filters,
+            const std::vector<int>&         kernel_size,
+            const std::vector<int>&         strides,
+            const boost::optional<double>&  batch_norm_momentum,
+            const std::string&              activation,
+            const boost::optional<double>&  dropout_rate,
+            const double&                   learning_rate);
     };
 
     GANImpl(
         const Params&           discriminator_params,
         const Params&           generator_params,
-        int                     generator_initial_dense_layer_size,
+        const std::vector<int>& generator_initial_dense_layer_size,
         const std::vector<int>& generator_upsample,
         const std::string&      optimizer,
         int                     z_dim,
@@ -41,14 +43,14 @@ public:
 private:
     Params              discriminator_params_;
     Params              generator_params_;
-    int                 generator_initial_dense_layer_size_;
+    std::vector<int>    generator_initial_dense_layer_size_;
     std::vector<int>    generator_upsample_;
     std::string         optimizer_;
     int                 z_dim_;
     torch::Device       device_;
     
     void build();
-    void build_discriminator();
+    torch::nn::Sequential build_discriminator();
     void build_generator();
     void build_adversarial();
 };
